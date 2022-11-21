beb2000dd0392e471b0514581a16fd496d6e8a72
Naumenko Pavel <pavelnaumenkowork@gmail.com>
2022-11-21 12:26:32 +0300

    3-6-5

--- for_fun/dashboard/urls.py
+++ for_fun/dashboard/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'dashboard'
+
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('group_list', views.group_posts, name='group_list'),
 ]
--- for_fun/dashboard/views.py
+++ for_fun/dashboard/views.py
@@ -13,9 +13,12 @@ def index(request):
 
 
 def group_posts(request):
+    text2 = ('Вчера Крокодил<br>улыбнулся так злобно,<br>Что мне до сих '
+            'пор<br>за него неудобно.<br><i>Рената Муха</i>')
     text = 'Здесь будет информация о группах проекта Yatube'
     template = 'dashboard/group_list.html'
     context = {
         'text': text,
+        'text2': text2
     }
     return render(request, template, context)
